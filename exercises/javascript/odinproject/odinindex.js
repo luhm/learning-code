c9509c12d8b84a2302d59a8051cfcc77bbf9943d
Luiza Moretti <luh.moretti@Luizas-MacBook-Pro.local>
2024-12-22 16:54:12 -0300

    update o conteudo com o enunciado das questões

--- exercises/javascript/odinproject/odinindex.js
+++ exercises/javascript/odinproject/odinindex.js
@@ -1,4 +1,6 @@
+// Exercícios tirados de https://www.theodinproject.com/lessons/foundations-function-basics
 
+// Write a function called add7 that takes one number and returns that number + 7.
 function add7(num) {
     return num + 7
 }
@@ -9,6 +11,7 @@ console.log (`O resultado é ${add7Result}`)
 
 ///////////////////////////
 
+// Write a function called multiply that takes 2 numbers and returns their product.
 function multiply(numA, numB) {
     return numA*numB
 }
