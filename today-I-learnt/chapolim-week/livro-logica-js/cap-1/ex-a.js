de3116d1256e6c6f42b0fdf6a0e139b81bce0783
Fernanda Vilela <fegvilela@gmail.com>
2025-02-05 10:05:19 -0300

    ex-a com funcao

--- today-I-learnt/chapolim-week/livro-logica-js/cap-1/ex-a.js
+++ today-I-learnt/chapolim-week/livro-logica-js/cap-1/ex-a.js
@@ -1,9 +1,14 @@
 let apartamento = Number(prompt("Qual o número do seu apartamento?"))
 
-let viz1 = apartamento - 1
-let viz2 = apartamento + 1
+function pegarVizinhoMenor(apartamento) {
+  return apartamento - 1
+}
+
+function pegarVizinhoMaior(apartamento) {
+  return apartamento + 1
+}
+
+let viz1 = pegarVizinhoMenor(apartamento)
+let viz2 = pegarVizinhoMaior(apartamento)
 
 alert("Seus vizinhos mais próximos são: " + viz1 + " e " + viz2)
-
-// DEU CERTO! Usei o devtools do navegador pra testar
-
